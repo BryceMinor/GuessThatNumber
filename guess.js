475cd62bf4bdfe82b825684575af19f329ef5d47
Bryce Minor <bryce.minor7@gmail.com>
2021-10-12 12:59:33 -0500

    Set up user first guess including removing an attempt.

--- guess.js
+++ guess.js
@@ -48,7 +48,26 @@ while (restartGame){
         attempts = parseInt(prompt(enterNumText));
     }
 
-    
+    // Asks user to enter a quess in the range that they set.
+    guess = prompt(`Please enter a guess from 1 to ${rangeNum}. You have ${attempts} attempt(s) left:`)
+
+    // Continues looping until the user guesses the correct number or runs out of attempts (NOTE: Loops un til BREAK keyword is run)
+    while (true){
+        // Attempts to convert the user's guess into a number
+        guess = parseInt(guess);
+
+        // Verifies the user's guess is a number greater than zero as well as a number within the range set by the user
+        while (!guess || guess < 1 || guess > rangeNum){
+            guess = parseInt(prompt(`Please enter a number from 1 to ${rangeNum}`));
+        }
+
+        // Removes an attempt
+        attempts--;
+
+        break;
+    }
+
+
 
     break;
 }
